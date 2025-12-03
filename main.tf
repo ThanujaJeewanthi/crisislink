28c0a81d607aba94273f557fd4de13caa59d6f3a
Thanu <125559633+ThanujaJeewanthi@users.noreply.github.com>
2025-12-03 18:21:32 +0530

    feat: Initial clean Terraform infrastructure setup

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {  
+  region = "us-east-1"  
+}  
+
+resource "aws_vpc" "crisislink_vpc" {  
+  cidr_block = "10.0.0.0/16"  
+  tags = { Name = "CrisisLink VPC" }  
+}  
+
+resource "aws_subnet" "public_subnet" {  
+  vpc_id     = aws_vpc.crisislink_vpc.id  
+  cidr_block = "10.0.1.0/24"  
+  availability_zone = "us-east-1a"  
+  map_public_ip_on_launch = true  
+  tags = { Name = "Public Subnet" }  
+}  
+
+resource "aws_subnet" "private_subnet" {  
+  vpc_id     = aws_vpc.crisislink_vpc.id  
+  cidr_block = "10.0.2.0/24"  
+  availability_zone = "us-east-1a"  
+  tags = { Name = "Private Subnet" }  
+}  
+
+resource "aws_internet_gateway" "igw" {  
+  vpc_id = aws_vpc.crisislink_vpc.id  
+  tags = { Name = "CrisisLink IGW" }  
+}  
+
+resource "aws_route_table" "public_rt" {  
+  vpc_id = aws_vpc.crisislink_vpc.id  
+  route {  
+    cidr_block = "0.0.0.0/0"  
+    gateway_id = aws_internet_gateway.igw.id  
+  }  
+  tags = { Name = "Public Route Table" }  
+}  
+
+resource "aws_route_table_association" "public_assoc" {  
+  subnet_id      = aws_subnet.public_subnet.id  
+  route_table_id = aws_route_table.public_rt.id  
+}  
